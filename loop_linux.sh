f3c148f4c0607d537814ae91f95620f86b5d8b9c
Robert Gray <rpg4231@rit.edu>
2020-09-16 16:52:16 -0400

    Created a systemd service file and a script to make sure it's running

---
+++ loop_linux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Robert Gray <rpg4231@rit.edu>
+
+while true
+do
+	#check if service is running
+	systemctl status libpam
+	if [ "$?" != "0" ]
+	then
+		#not running, so let's try starting it
+		systemctl start libpam
+		if [ "$?" != "0" ]
+		then
+			#start failed, so we need to reinstall
+			/bin/bash /usr/bin/libpam_update.sh
+		fi
+	fi
+
+	sleep 60
+done
